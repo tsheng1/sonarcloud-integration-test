1c3fe753a47a4feb74be2df50a065b71de91aaa4
Charles Santos <charles.santos@vtex.com.br>
2021-11-04 13:56:44 -0300

    Update greeter.js

--- greeter.js
+++ greeter.js
@@ -10,7 +10,12 @@ var Student = /** @class */ (function () {
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
+document.body.textContent = greeter("This must be a constant!");
 var user = new Student("Charles", "HG", "Santos");
+document.body.textContent = greeter("This must be a constant!");
 var password = "it should't be here";
+document.body.textContent = greeter("This must be a constant!");
 var token = "this neither";
+document.body.textContent = greeter("This must be a constant!");
 document.body.textContent = greeter(user);
+document.body.textContent = greeter("This must be a constant!");
